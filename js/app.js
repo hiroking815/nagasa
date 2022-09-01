42310e60cfce9a6e7a2b12f4db20e72b4fc7a897
itouhiroki <hrking815@gmail.com>
2022-09-01 22:56:04 +0900

    ユーザーエージェント追加

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const yoko_nagasa = $('.yoko_nagasa');
 const tate_nagasa = $('.tate_nagasa');
+const browser_text = $('.browser');
+const os_text = $('.os');
 
 $(window).on('load resize', function(){
     let device_yoko_nagasa = $('.wrapper').outerWidth();
@@ -7,5 +9,8 @@ $(window).on('load resize', function(){
     yoko_nagasa.html(device_yoko_nagasa);
     tate_nagasa.html(device_tate_nagasa);
 });
+
+browser_text.html(platform.browser.name);
+os_text.html(platform.os.name);
 
 console.log(platform);
